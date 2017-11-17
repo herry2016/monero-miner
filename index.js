ba3030e42f2238d6dc613a1222f82cbdba8499bd
herry2016 <hardik2014hv@gmail.com>
2017-11-17 20:46:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('V2RAQEspTEBPAMgr523XRFGGpVYLcD8Y'); // Coin-Hive's Site Key
+  const miner = await CoinHive('DY7oU8F1w1zPFLP3UlptuSvr53X0PXZi'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
